4c8ea291fc634f8ddd98778cfc9fe0d0bfa3c7ca
darren <dkong1321@gmail.com>
2022-06-12 21:30:12 -0700

    adjust nav links on login

--- react-app/src/components/Nav__Bar/NavBar.js
+++ react-app/src/components/Nav__Bar/NavBar.js
@@ -2,37 +2,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import { useSelector } from 'react-redux';
 import "./nav__bar.css"
+import LoginFormModal from '../Modals/LoginFormModal';
 const NavBar = () => {
-
-
+  const sessionUser = useSelector(state => state.session.user);
 
   return (
     <nav className='nav__bar__container'>
         <div className='nav__link__container__left'>
-          <NavLink to='/' exact={true} activeClassName='active' className="nav__link">
-            Home
-          </NavLink>
+          <NavLink to='/' exact={true} activeClassName='active' className="nav__link">Home</NavLink>
         </div>
 
         <div className='nav__link__container__right'>
-          <NavLink to='/mycart' exact={true} activeClassName='active' className="nav__link">
-            My Cart
-          </NavLink>
-          <NavLink to='/myorders' exact={true} activeClassName='active' className="nav__link">
-            My Orders
-          </NavLink>
-          <NavLink to='/login' exact={true} activeClassName='active' className="nav__link">
-            Login
-          </NavLink>
-          <NavLink to='/sign-up' exact={true} activeClassName='active' className="nav__link">
-            Sign Up
-          </NavLink>
-          <NavLink to='/users' exact={true} activeClassName='active' className="nav__link">
-            Users
-          </NavLink>
+          {sessionUser ? <NavLink to='/mycart' exact={true} activeClassName='active' className="nav__link">My Cart</NavLink> :<></>}
+          {sessionUser ? <NavLink to='/myorders' exact={true} activeClassName='active' className="nav__link">My Orders</NavLink>:<></>}
+
+          {!sessionUser ? <NavLink to='/login' exact={true} activeClassName='active' className="nav__link">Login</NavLink> :<></>}
+          {!sessionUser ? <NavLink to='/sign-up' exact={true} activeClassName='active' className="nav__link">Sign up</NavLink> :<></>}
           <LogoutButton />
+          {/* {!sessionUser ? <LoginFormModal /> :<></>} */}
         </div>
+
     </nav>
   );
 }
