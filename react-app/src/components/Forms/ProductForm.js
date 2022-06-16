9982a6d63ab138fe47083a7217f68072d67cbfbc
darren <dkong1321@gmail.com>
2022-06-16 16:05:49 -0700

    loading gif

--- react-app/src/components/Forms/ProductForm.js
+++ react-app/src/components/Forms/ProductForm.js
@@ -2,16 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { createProduct, getAllProducts } from "../../store/product";
 import { useHistory } from "react-router-dom";
-
+import loadingGif from "./279032259_1047884352471825_57850663476320908_n.gif"
 const ProductForm = ({setShowModal}) => {
     const dispatch = useDispatch()
-    // const [ isLoaded, setIsLoaded ] = useState(false)
     const [productName, setProductName] = useState("");
     const [price, setPrice] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] = useState(null);
+    const [productLoading, setProductLoading] = useState(false)
     const history = useHistory()
-    // const products =Object.values(useSelector((state)=> state.products))
     const user =useSelector((state)=> state.session.user)
 
     const [errorName, setErrorName] = useState([])
@@ -40,7 +39,6 @@ const ProductForm = ({setShowModal}) => {
             errorNameValidation.push("Product name cannot be more than 30 characters")
 
         }
-
         if(!description.length){
             errorDescriptionValidation.push("Product description cannot be empty")
 
@@ -57,11 +55,9 @@ const ProductForm = ({setShowModal}) => {
             errorPriceValidation.push("Price is seems too large please check and submit")
 
         }
-
         if(image !=="image/jpeg" || image !=="image/png" || image !=="image/jpg"){
             errorImageValidation.push("Invalid file type")
         }
-
 
         if (errorNameValidation.length || errorDescriptionValidation.length || errorPriceValidation.length){
             setErrorName(errorNameValidation)
@@ -78,9 +74,12 @@ const ProductForm = ({setShowModal}) => {
             description,
             image
         }
-        dispatch(createProduct(data))
-        history.push('/products')
+
+        await setProductLoading(true)
+        await dispatch(createProduct(data))
+        await setProductLoading(false)
         setShowModal(false)
+        history.push('/products')
 
     }
 
@@ -91,31 +90,39 @@ const ProductForm = ({setShowModal}) => {
 
     return(
         <div>
-            <div>New Product Form</div>
-            <form onSubmit={makeNewProduct}>
-                <div>
-                    <label>Product Name</label>
-                    {errorName ? <div>{errorName}</div> : <></>}
-                    <input value={productName} onChange={e => setProductName(e.target.value)} placeholder="enter product name"/>
-                </div>
-                <div>
-                    <label>Description</label>
-                    {errorDescription ? <div>{errorDescription}</div> : <></>}
-                    <input value={description} onChange={e => setDescription(e.target.value)} placeholder="enter product name"/>
-                </div>
-                <div>
-                    <label>Price</label>
-                    {errorPrice ? <div>{errorPrice}</div> : <></>}
-                    <input type='number' value={price} onChange={e => setPrice(e.target.value)} pattern='[0-9]+(\\.[0-9][0-9]?)?' placeholder="0.00" min="0.00" step="0.01"/>
-                </div>
-                <div>
-                    <label>Add Image</label>
-                    {errorImage ? <div>{errorImage}</div> : <></>}
-                    <input required type ="file" accept="image/*" onChange={updateImage}/>
-                </div>
 
-                <button type="submit">Submit</button>
-            </form>
+            {productLoading && <div>
+                <div>Loading...</div>
+                <img className="loading__gif" src={loadingGif}></img>
+                </div>
+                }
+            <div>
+                <div>New Product Form</div>
+                <form onSubmit={makeNewProduct}>
+                    <div>
+                        <label>Product Name</label>
+                        {errorName ? <div>{errorName}</div> : <></>}
+                        <input value={productName} onChange={e => setProductName(e.target.value)} placeholder="enter product name"/>
+                    </div>
+                    <div>
+                        <label>Description</label>
+                        {errorDescription ? <div>{errorDescription}</div> : <></>}
+                        <input value={description} onChange={e => setDescription(e.target.value)} placeholder="enter product name"/>
+                    </div>
+                    <div>
+                        <label>Price</label>
+                        {errorPrice ? <div>{errorPrice}</div> : <></>}
+                        <input type='number' value={price} onChange={e => setPrice(e.target.value)} pattern='[0-9]+(\\.[0-9][0-9]?)?' placeholder="0.00" min="0.00" step="0.01"/>
+                    </div>
+                    <div>
+                        <label>Add Image</label>
+                        {errorImage ? <div>{errorImage}</div> : <></>}
+                        <input required type ="file" accept="image/*" onChange={updateImage}/>
+                    </div>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
